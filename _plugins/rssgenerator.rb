8a52e1443cec52562b35902e2be1961db8ee36f6
Jonathon Mah <me@JonathonMah.com>
2013-10-24 09:53:41 -0700

    RSS uses subtitle for description, if possible
    If not, uses the raw, non-HTML-ified excerpt. Markdown parsing begone.

--- _plugins/rssgenerator.rb
+++ _plugins/rssgenerator.rb
@@ -35,8 +35,6 @@ module Jekyll
     def generate(site)
       require 'rss'
 
-      parser = get_markdown_parser(site.config)
-
       # Create the rss with the help of the RSS module
       rss = RSS::Maker.make("2.0") do |maker|
         maker.channel.title = site.config['name']
@@ -53,7 +51,7 @@ module Jekyll
           maker.items.new_item do |item|
             item.title = post.title
             item.link = site_url_slash + remove_leading_slash(post.url)
-            item.description = parser.convert(post.excerpt)
+            item.description = post.data['subtitle'] || post.excerpt.strip
             item.updated = post.date
           end
         end
@@ -111,28 +109,5 @@ module Jekyll
     def ensure_dir(path)
       FileUtils.mkdir_p(path)
     end
-
-    # Gets a parser object for the parser specified in the configuration
-    #
-    # config - the site's configuration hash
-    #
-    # Returns a parser or raises exception if one isn't found
-    def get_markdown_parser(config)
-      return case config['markdown']
-        when 'redcarpet'
-          Jekyll::Converters::Markdown::RedcarpetParser.new config
-        when 'kramdown'
-          Jekyll::Converters::Markdown::KramdownParser.new config
-        when 'rdiscount'
-          Jekyll::Converters::Markdown::RDiscountParser.new config
-        when 'maruku'
-          Jekyll::Converters::Markdown::MarukuParser.new config
-        else
-          STDERR.puts "Invalid Markdown processor: #{config['markdown']}"
-          STDERR.puts "  Valid options are [ maruku | rdiscount | kramdown | redcarpet ]"
-          raise FatalException.new("Invalid Markdown process: #{config['markdown']}")
-      end
-    end
-
   end
 end
